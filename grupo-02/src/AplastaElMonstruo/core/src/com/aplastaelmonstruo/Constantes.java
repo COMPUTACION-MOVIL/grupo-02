1d9047cd2c4786c4859e8d534afdf3d705fe0a16
Sebastian Montoya J <yatocapo@gmail.com>
2016-10-11 21:41:09 -0500

    Avance No. 2

--- grupo-02/src/AplastaElMonstruo/core/src/com/aplastaelmonstruo/Constantes.java
+++
@@ -1,8 +0,0 @@
-package com.aplastaelmonstruo;
-
-// En esta clase guardaremos todas las constantes que necesitaremos a lo largo del juego
-public class Constantes
-{
-    public static final float ancho_vista = 5.0F; // El ancho visible del mundo seran 5 metros
-    public static final float alto_vista = 5.0F; // El alto visible del mundo tambien seran 5 metros
-}
